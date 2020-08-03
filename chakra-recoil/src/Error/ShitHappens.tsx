6029018aa775dca449dce3608b63e94419d42be3
Utilisateur <franck.ledoux.pro@gmail.com>
2020-08-03 21:57:26 +0200

    Better error message

--- chakra-recoil/src/Error/ShitHappens.tsx
+++ chakra-recoil/src/Error/ShitHappens.tsx
@@ -1,26 +1,27 @@
 import React, { FunctionComponent } from "react";
-import { Box, useColorMode, Text } from "@chakra-ui/core";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Text,
+  Box,
+} from "@chakra-ui/core";
 
-const color = { light: "black", dark: "white" };
-const bgColor = { light: "red", dark: "black" };
-
-export const ShitHappens: FunctionComponent = () => {
-  const { colorMode } = useColorMode();
-  return (
-    <Box
-      background={bgColor[colorMode]}
-      color={color[colorMode]}
-      fontWeight="bold"
-      padding="20"
+export const ShitHappens: FunctionComponent = () => (
+  <Box borderColor="black" borderWidth="1px" padding="1" marginX="2">
+    <Alert
+      status="error"
+      flexDirection="column"
+      justifyContent="center"
+      textAlign="center"
     >
-      <Text fontSize="4xl" fontWeight="bold">
+      <AlertIcon />
+      <AlertTitle mt="3">Oups, Error!</AlertTitle>
+      <AlertDescription mt="5">
         Sorry, Something wrong Happen.
-        <br />
-        <br />
-      </Text>
-      <Text fontSize="3xl">
-        You should relod the app.
-      </Text>
-    </Box>
-  );
-};
+        <Text mt="2">You should relod the app.</Text>
+      </AlertDescription>
+    </Alert>
+  </Box>
+);
