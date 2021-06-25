a853d33e125fc2fe59ed44a174f8225ae965d2f4
Utilisateur <franck.ledoux.pro@gmail.com>
2021-06-25 22:29:38 +0200

    add couter

--- chakra-recoil/src/state/posts/atoms.ts
+++ chakra-recoil/src/state/posts/atoms.ts
@@ -3,3 +3,5 @@ import { atom } from "recoil";
 export const filterPostsState = atom<string | undefined>({ key: "postsFilter", default: undefined });
 
 export const loadedPostsDateState = atom<Date>({ key: "loadedPostsDate", default: new Date() });
+
+export const postsCountState = atom<number>({ key: 'postsCount', default: 0 })
--- chakra-recoil/src/state/posts/functions.ts
+++ chakra-recoil/src/state/posts/functions.ts
@@ -1,11 +1,17 @@
 import { selector, selectorFamily } from "recoil";
 import { PostsApi } from "../../api/posts";
 import { Post } from "../../model";
-import { filterPostsState } from "./atoms";
+import { filterPostsState, postsCountState } from "./atoms";
 
 export const postsState = selector<Post[]>({
   key: "postsList",
   get: async () => await PostsApi.getAll(),
+});
+
+export const postsState2 = selectorFamily<Post[], number>({
+  key: "postsList2",
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  get(counter: number) { return async () => await PostsApi.getAll() },
 });
 
 export const filteredPostsState = selector<Post[]>({
@@ -28,3 +34,13 @@ export const postByIdState = selectorFamily<Post | undefined, number>({
 });
 
 const canonicalString = (value: string | null | undefined) => value?.trim().toLowerCase()
+
+export const filteredPostsState2 = selector<Post[]>({
+  key: "postsFiltered",
+  async get({ get }) {
+    const count = get(postsCountState)
+    const posts = get(postsState2(count));
+    const filter = canonicalString(get(filterPostsState));
+    return filter === undefined ? posts : posts.filter(post => canonicalString(post.title)?.includes(filter))
+  },
+});
